7030a33bf5ae6d70b64bd2ab934cbdc0362be5dc
ches <ches@itu.dk>
2023-08-30 15:54:55 +0200

    comments

--- pythoncode/week1/stablematching.py
+++ pythoncode/week1/stablematching.py
@@ -10,22 +10,26 @@ from collections import deque
 #INPUT
 n, m = map(int, input().split())
 P = {} #proposers
-R = {} #rejectors
+R = {} #rejecters
 for _ in range(int(n/2)): #n/2 proposers
     names = input().split()
     P[names[0]] = deque(names[1:]) 
     P[names[0]].reverse() 
-for _ in range(int(n/2)): #n/2 rejectors
+for _ in range(int(n/2)): #n/2 rejecters
     names = input().split()
     R[names[0]] = {} 
     for i in range(1, len(names)): 
         R[names[0]][names[i]] = i 
 
-#test data structure
+#test data structure on sample input
 #print(P)
+#{'Chandler': deque(['Phoebe', 'Rachel', 'Monica']), 'Joey': deque(['Monica', 'Phoebe', 'Rachel']), 'Ross': deque(['Phoebe', 'Rachel'])}
+#We can find each proposers next choice in O(1) time with pop().
 #print(R)
+#{'Monica': {'Chandler': 1, 'Joey': 2}, 'Phoebe': {'Joey': 1, 'Ross': 2, 'Chandler': 3}, 'Rachel': {'Ross': 1, 'Joey': 2, 'Chandler': 3}}
+#We can easily compare rejecters preferences for each proposer in O(1) time.
 
-#Gale-shapley algorithm execution
+#Gale-shapley algorithm
 
 
 #OUTPUT
