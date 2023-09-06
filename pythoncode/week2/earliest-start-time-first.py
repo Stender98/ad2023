46f9e2768c9cabb2838360bbba2de451559df214
ches <ches@itu.dk>
2023-09-06 15:47:34 +0200

    debugging prints

--- pythoncode/week2/earliest-start-time-first.py
+++ pythoncode/week2/earliest-start-time-first.py
@@ -10,27 +10,28 @@ for _ in range(n):
     (start_time, end_time) = map(int, input().split())
     activities.append((start_time, end_time))
 activities.sort(key=lambda x: x[0])
+print("activities: ")
+print(activities)
 
 classrooms = PriorityQueue(k) 
-for _ in range(k-1): 
-    classrooms.put(0) 
-classrooms.put(activities[0][1])
+for i in range(k): 
+    classrooms.put(activities[i][1])
 
 #GREEDY ALGORITHM
-scheduled = 1
-#print("status PQ: ")
-#print(classrooms.queue)
-for i in range(1, n):
-    #print("activity: ")
-    #print((activities[i][0], activities[i][1])) 
-    if activities[i][0] >= classrooms.queue[0]: 
+scheduled = k
+print("status PQ: ")
+print(classrooms.queue)
+for j in range(k, n):
+    print("activity: ")
+    print((activities[j][0], activities[j][1])) 
+    if activities[j][0] >= classrooms.queue[0]: 
         classrooms.get() 
-        classrooms.put(activities[i][1]) 
+        classrooms.put(activities[j][1]) 
         scheduled += 1 
-        #print("chosen")
-    #print("status PQ: ")
-    #print(classrooms.queue) 
+        print("chosen")
+    print("status PQ: ")
+    print(classrooms.queue) 
 
 #OUTPUT
-#print("Number of activities: ")
+print("Number of activities: ")
 print(scheduled)
